c2c43a12c4d0180158f84e9dcf7e30e785915f1e
Filippo Guggino <guggio1997@gmail.com>
2025-02-23 23:32:56 +0100

    hello world opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,48 @@
-#include <raylib.h>
+#include <iostream>
+
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 int main(void)
 {
-    InitWindow(1280, 720, "WebAssembly");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    Font font = LoadFont("resources/fonts/nokiafc22.ttf");
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawTextEx(font, "Hello, world", Vector2 { 190, 200 }, 20, 2, LIGHTGRAY);
-
-        EndDrawing();
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
     }
 
-    UnloadFont(font);
+    glViewport(0, 0, 800, 600);
 
-    CloseWindow();
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 
     return 0;
 }
--- wasm.sh
+++ wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/xubuntu/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web -B build.web
-cmake --build build.web/ -j12
+cmake -DCMAKE_TOOLCHAIN_FILE=/home/fguggino/workspace/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web -B build.web -GNinja
+cmake --build build.web/
