efbff2b9e9811aca8f7281e89af2cd49aca1946c
Filippo Guggino <guggio1997@gmail.com>
2025-02-24 23:36:53 +0100

    fix: project now compiles on wasm too

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GL_EXT_PROTOTYPES
+#define EGS_EGLEXT_PROTOTYPES
+#else
 #include "glad/glad.h"
+#endif
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -8,13 +14,24 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void main_loop(void* ctx)
+{
+    GLFWwindow* window = (GLFWwindow*)ctx;
+
+    glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
 int main(void)
 {
+    emscripten_log(EM_LOG_CONSOLE, "main");
+
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
@@ -24,23 +41,25 @@ int main(void)
     }
     glfwMakeContextCurrent(window);
 
+#ifdef __EMSCRIPTEN__
+#else
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+#endif
 
     glViewport(0, 0, 800, 600);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop_arg(main_loop, (void*)window, 0, true);
+#else
     while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
+        main_loop((void*)window);
     }
+#endif
 
     glfwTerminate();
 
