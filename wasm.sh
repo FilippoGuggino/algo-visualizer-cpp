d5b435b11175c9261d5ca7397ed73e45e759d8e7
Filippo Guggino <filippo.guggino@alkeria.com>
2025-05-08 17:29:42 +0200

    use EMSDK_ROOT environment variable in wasm.sh

--- wasm.sh
+++ wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/fguggino/workspace/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web -B build.web -GNinja
+cmake -DCMAKE_TOOLCHAIN_FILE=$EMSDK_ROOT/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web -B build.web -GNinja
 cmake --build build.web/
