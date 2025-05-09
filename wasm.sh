46b29f281a632e046a4f1e01a6b4c037b6724ee8
Filippo Guggino <filippo.guggino@alkeria.com>
2025-05-09 17:15:16 +0200

    formatted CMakeLists.txt

--- wasm.sh
+++ wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$EMSDK_ROOT/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web -B build.web -GNinja
-cmake --build build.web/
+cmake --build build.web
